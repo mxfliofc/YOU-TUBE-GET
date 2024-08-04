3e8a5bcded94581600d8a4af5b273b59eafc5e7a
Mxflixa Support <126412851+mxfliofc@users.noreply.github.com>
2024-08-04 20:28:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template_string
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
@@ -8,6 +8,13 @@ def index():
 
 @app.route('/get-video/mxfliofc-vip/<path:url>')
 def redirect_video(url):
+    # Verifica se a URL começa com 'http:' ou 'https:' e ajusta conforme necessário
+    if url.startswith("http:/"):
+        url = url.replace("http:/", "http://")
+    elif url.startswith("https:/"):
+        url = url.replace("https:/", "https://")
+
+    # Verifica se a URL segue o formato desejado
     if url.startswith("https://wishonly.site/e/"):
         video_id = url.split("/")[-1]
         new_url = f"https://wishonly.site/f/{video_id}_x"
